250f811ab51a6314935f463c43254994133db8b7
xingdaomqb <mqb@mail.ustc.edu.cn>
2020-09-15 01:45:47 +0800

    add UserArgumentResolver to get MiaoshaUser from redis before controller

---
+++ src/main/java/com/mqb/seckill/config/UserArgumentResolver.java
@@ -0,0 +1,64 @@
+package com.mqb.seckill.config;
+
+import com.mqb.seckill.constants.MiaoshaUserConstants;
+import com.mqb.seckill.entity.MiaoshaUser;
+import com.mqb.seckill.service.MiaoshaUserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Resource
+    private MiaoshaUserService userService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        Class<?> parameterType = methodParameter.getParameterType();
+        return parameterType == MiaoshaUser.class;
+    }
+
+    /**
+     * 从request中获取token，根据token查询redis，并注入到controller入参中
+     * @param methodParameter
+     * @param modelAndViewContainer
+     * @param nativeWebRequest
+     * @param webDataBinderFactory
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        HttpServletRequest request = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
+        HttpServletResponse response = nativeWebRequest.getNativeResponse(HttpServletResponse.class);
+        String paramToken = request.getParameter(MiaoshaUserConstants.COOKIE_NAME_TOKEN);
+        String cookieToken = getCookieValue(request, MiaoshaUserConstants.COOKIE_NAME_TOKEN);
+        if (StringUtils.isEmpty(paramToken) && StringUtils.isEmpty(cookieToken)) {
+            return null;
+        }
+        String token = StringUtils.isEmpty(paramToken) ? cookieToken : paramToken;
+        MiaoshaUser user = userService.getByToken(response, token);
+        return user;
+    }
+
+    // 从request中获取cookie中token
+    private String getCookieValue(HttpServletRequest request, String cookieNameToken) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookieNameToken.equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/com/mqb/seckill/controller/GoodsController.java
+++ src/main/java/com/mqb/seckill/controller/GoodsController.java
@@ -1,17 +1,12 @@
 package com.mqb.seckill.controller;
 
-import com.mqb.seckill.constants.MiaoshaUserConstants;
 import com.mqb.seckill.entity.MiaoshaUser;
 import com.mqb.seckill.service.MiaoshaUserService;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
@@ -20,18 +15,15 @@ public class GoodsController {
     @Resource
     private MiaoshaUserService miaoShaUserService;
 
-
+    /**
+     *
+     * @param model
+     * @param miaoshaUser  inject from UserArgument Resolver in config package
+     * @return
+     */
     @RequestMapping("/to_list")
-    public String toList(Model model,
-                         HttpServletResponse response,
-                         @CookieValue(value = MiaoshaUserConstants.COOKIE_NAME_TOKEN,required = false)String cookieToken,
-                         @RequestParam(value = MiaoshaUserConstants.COOKIE_NAME_TOKEN,required = false)String paramToken){
-        if(StringUtils.isEmpty(cookieToken)&&StringUtils.isEmpty(paramToken)){
-            return "login";
-        }
-        String token = StringUtils.isEmpty(paramToken)?cookieToken:paramToken;
-        MiaoshaUser miaoshaUser = miaoShaUserService.getByToken(response, token);
-        model.addAttribute("user",miaoshaUser);
+    public String toList(Model model, MiaoshaUser miaoshaUser) {
+        model.addAttribute("user", miaoshaUser);
         return "goods_list";
     }
 }
